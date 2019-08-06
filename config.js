9532ea18c8d080c5a62a3e6788d1d095ce841eb9
Sunny Hu <shshsunny@126.com>
2019-08-06 20:02:47 +0800

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+window.config = {
+  server: 'https://api.github.com',
+  siteName: 'Eyas Liu',
+  navbar: [
+    {
+      label: '首页',
+      link: '/'
+    }, {
+      label: '博客',
+      link: '/blog'
+    }
+  ],
+  github: {
+    user: 'kenese-party',
+    repo: 'beta'
+  }
+}
